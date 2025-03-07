a642499ac655f09a27cf2c6411989ed6bedac248
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-03-07 18:36:50 +0900

    수열의 개수 및 각 수열의 항의 개수 출력 형식 올바르게 수정

--- Repeat-Extract.jl
+++ Repeat-Extract.jl
@@ -51,7 +51,8 @@ end
 
 println("========================================================================================")
 
+println("The Number Of Sequences : " * string(length(All_Prime_Sequence)))
+
 for i in 1:length(All_Prime_Sequence)
-  println("The Number Of Sequences : " * string(length(All_Prime_Sequence)))
   print(string(length(All_Prime_Sequence[i])) * " ")
 end
